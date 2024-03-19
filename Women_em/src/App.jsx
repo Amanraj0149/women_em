21ff9fd3183206d29809344e11d17dc7e5f69bf3
Ritesh verma <98600361+riteshkv57@users.noreply.github.com>
2024-03-19 15:57:34 +0530

    Update App.jsx

--- Women_em/src/App.jsx
+++ Women_em/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import Header from "./components/Header.jsx"
-import './App.css'
+import React from 'react';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <Header />
+      <Header />
+      {/* <Footer /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
