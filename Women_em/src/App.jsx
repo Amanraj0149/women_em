01b346c6c9a16ce7d3bb0d84c88dc93e3bb89bfd
Ritesh verma <98600361+riteshkv57@users.noreply.github.com>
2024-03-19 19:44:03 +0530

    Update App.jsx

--- Women_em/src/App.jsx
+++ Women_em/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Upheader from './components/Upheader.jsx';
 import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
+import Footer from './components/footer.jsx';
 import Bfoot from './components/Bfoot.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
