8696d6d78c7ffec17e1e6b9b95982126153a7748
Ritesh verma <98600361+riteshkv57@users.noreply.github.com>
2024-03-20 23:39:00 +0530

    Update App.jsx

--- Women_em/src/App.jsx
+++ Women_em/src/App.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
-import Upheader from './components/Upheader.jsx';
 import Header from './components/Header.jsx';
-import Footer from './components/footer.jsx';
+import Footer from './components/Footer.jsx';
+import Upheader from './components/Upheader.jsx';
+import Login from './components/Login.jsx';
 import Bfoot from './components/Bfoot.jsx';
+import Home from './components/Home.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter,RouterProvider} from 'react-router-dom';
+import Banner from './components/Banner.jsx';
+import Chatbot from './components/Chatbot.jsx';
+import './App.css'
 
 function App() {
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <><Banner /></>
+  },
+  {
+    path:"/login",
+    element: <><Login /></>
+  },
+  {
+    path:"/contactus",
+    element: <><Upheader /><Header /><Footer /><Bfoot /><Chatbot /></>
+  },
+])
+
+
+
+
+
+
   return (
     <>
-      <Upheader />
+    {/* <Upheader />
       <Header />
+      <Login />
       <Footer />
-      <Bfoot />
+      <Bfoot /> */}
+      <RouterProvider router={router} />
     </>
   );
 }
